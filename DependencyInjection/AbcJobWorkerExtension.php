e2df77221b43e8647c1b9078659700ec26b50199
Hannes Schulz <schulz@daten-bahn.de>
2019-09-25 22:12:25 +0200

    fixed extension class name

--- DependencyInjection/AbcJobWorkerExtension.php
+++ DependencyInjection/AbcJobWorkerExtension.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class AbcWorkerExtension extends Extension
+class AbcJobWorkerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
