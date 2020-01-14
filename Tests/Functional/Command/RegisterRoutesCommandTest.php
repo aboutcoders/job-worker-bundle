d4f7c97133c091cbbea495891470c6097cab8df6
hsc <hsc@fmg-guides.com>
2020-01-14 09:46:43 +0100

    added test for RegisterRoutesCommand

---
+++ Tests/Functional/Command/RegisterRoutesCommandTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Abc\JobWorkerBundle\Tests\Functional\Command;
+
+use Abc\Job\Broker\Route;
+use Abc\JobWorkerBundle\Tests\Functional\KernelTestCase;
+use GuzzleHttp\Psr7\Response;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class ProcessJobCommandTest extends KernelTestCase
+{
+    public function setUp(): void
+    {
+        static::bootKernel();
+    }
+
+    public function testExecute()
+    {
+        $application = new Application(static::$kernel);
+
+        $command = $application->find('abc:register:routes');
+
+        $input = [
+            'command' => $command->getName(),
+        ];
+        $options = ['verbosity' => OutputInterface::VERBOSITY_DEBUG];
+
+        $this->mockRouteClientResponse();
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute($input, $options);
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+
+        $this->assertContains('Consumption has started', $output);
+
+        /** @var \Abc\Job\Interop\JobConsumer $jobConsumer */
+        $jobConsumer = static::$container->get('abc.job.job_consumer');
+
+        $this->assertEquals(['jobA', 'jobB'], $jobConsumer->getJobs());
+    }
+
+    protected function mockRouteClientResponse(): void
+    {
+        $route_A = new Route('jobA', 'queueName', 'replyTo');
+        $route_B = new Route('jobB', 'queueName', 'replyTo');
+
+        $json = json_encode([(object) $route_A->toArray(), (object) $route_B->toArray()]);
+
+        $response = new Response(200, [], $json);
+
+        $mockHandler = static::$container->get('app.http_route_client.mock_handler');
+        $mockHandler->append($response);
+        $mockHandler->append($response);
+    }
+}
